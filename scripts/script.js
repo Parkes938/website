89800e292c3a7dce9ffd8c68bf93e517e623109f
Rowan Griffin <rowangee@gmail.com>
2018-01-04 14:49:08 +0000

    Chrismas Work

--- scripts/script.js
+++ scripts/script.js
@@ -36,3 +36,22 @@ $(window).resize(function() {
     }
   }
 });
+
+	$('#contactform').one('submit',function(){
+        var first = encodeURIComponent($('#first').val());
+        var last = encodeURIComponent($('#last').val());
+		var email = encodeURIComponent($('#email').val());
+        var subject = encodeURIComponent($('#subject').val());
+		var message = encodeURIComponent($('#message').val());
+        var first = "entry.2093447866";
+        var last = "entry.951463637";
+		var email = "entry.993572906";
+        var subject = "entry.1392860080";
+		var message = "entry.1637301940";
+        var baseURL = 'https://docs.google.com/forms/d/e/1FAIpQLScv35MTP43cAWjy8sLdUQm8hAb12q63w6V4zL-Ui5PaUOOWNw/formResponse';
+        var submitRef = '&submit=7337784394560228092';
+        var submitURL = (https://docs.google.com/forms/d/e/1FAIpQLScv35MTP43cAWjy8sLdUQm8hAb12q63w6V4zL-Ui5PaUOOWNw/formResponse + first + "=" + first + "&" + last + "=" + last + "&" + email + "=" + email + "&" + subject + "=" + subject + "&" + message + "=" + message + submitRef);
+        console.log(submitURL);
+        $(this)[0].action=submitURL;
+        $('#input-feedback').text('Thank You!');
+    });
