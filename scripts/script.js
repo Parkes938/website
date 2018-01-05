617dff0cb748fc535c8ec13551b838e58d686905
Rowan Griffin <rowangee@gmail.com>
2018-01-05 13:54:52 +0000

    All css to one

--- scripts/script.js
+++ scripts/script.js
@@ -38,20 +38,20 @@ $(window).resize(function() {
 });
 
 	$('#contactform').one('submit',function(){
-        var first = encodeURIComponent($('#first').val());
-        var last = encodeURIComponent($('#last').val());
-		var email = encodeURIComponent($('#email').val());
-        var subject = encodeURIComponent($('#subject').val());
-		var message = encodeURIComponent($('#message').val());
+        var first = encodeURIComponent($('#firstfield').val());
+        var last = encodeURIComponent($('#lastfield').val());
+		var email = encodeURIComponent($('#emailfield').val());
+        var subject = encodeURIComponent($('#subjectfield').val());
+		var message = encodeURIComponent($('#messagefield').val());
         var first = "entry.2093447866";
         var last = "entry.951463637";
 		var email = "entry.993572906";
         var subject = "entry.1392860080";
 		var message = "entry.1637301940";
-        var baseURL = 'https://docs.google.com/forms/d/e/1FAIpQLScv35MTP43cAWjy8sLdUQm8hAb12q63w6V4zL-Ui5PaUOOWNw/formResponse';
+        var baseURL = 'https://docs.google.com/forms/d/e/1FAIpQLScv35MTP43cAWjy8sLdUQm8hAb12q63w6V4zL-Ui5PaUOOWNw/formResponse;
         var submitRef = '&submit=7337784394560228092';
-        var submitURL = (https://docs.google.com/forms/d/e/1FAIpQLScv35MTP43cAWjy8sLdUQm8hAb12q63w6V4zL-Ui5PaUOOWNw/formResponse + first + "=" + first + "&" + last + "=" + last + "&" + email + "=" + email + "&" + subject + "=" + subject + "&" + message + "=" + message + submitRef);
-        console.log(submitURL);
+        var submitURL = (https://docs.google.com/forms/d/e/1FAIpQLScv35MTP43cAWjy8sLdUQm8hAb12q63w6V4zL-Ui5PaUOOWNw/formResponse/first=firstfield&entry.last=lastfield&entry.email=emailfield&entry.subject=subjectfield&entry.message=messagefield&submit=7337784394560228092;
+		console.log(submitURL);
         $(this)[0].action=submitURL;
         $('#input-feedback').text('Thank You!');
     });
