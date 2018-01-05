331302270e909210c64c808a9eed4e055041c8bc
Rowan Griffin <rowangee@gmail.com>
2018-01-05 15:08:43 +0000

    Messing with Contact Form. AARGH

--- scripts/script.js
+++ scripts/script.js
@@ -1,57 +1,24 @@
-var distanceY;
-
-function init() {
-    window.addEventListener('scroll', function(e){
-            distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            shrinkOn = 50,
-            headerbar = document.querySelector("#nav-menu");
-            headerlogo = document.querySelector("#logo");
-        if (distanceY > shrinkOn) {
-            if (classie.has(headerlogo,"topLogo")) {
-                classie.remove(headerlogo,"topLogo");
-            }
-            if (classie.has(headerbar,"topNav")) {
-                classie.remove(headerbar,"topNav");
-            }
-        } else if(windowsize > 900){
-
-            classie.add(headerlogo,"topLogo");
-            classie.add(headerbar,"topNav");
-        }
-    });
-}
-window.onload = init();
-
-var windowsize = $(window).width();
-
-$(window).resize(function() {
-  windowsize = $(window).width();
-  if(windowsize<900)
-  {
-    if (classie.has(headerlogo,"topLogo")) {
-        classie.remove(headerlogo,"topLogo");
-    }
-    if (classie.has(headerbar,"topNav")) {
-        classie.remove(headerbar,"topNav");
-    }
-  }
+$(document).ready(function() {
+  console.log("Form Script Running");
 });
 
-	$('#contactform').one('submit',function(){
-        var first = encodeURIComponent($('#firstfield').val());
-        var last = encodeURIComponent($('#lastfield').val());
-		var email = encodeURIComponent($('#emailfield').val());
-        var subject = encodeURIComponent($('#subjectfield').val());
-		var message = encodeURIComponent($('#messagefield').val());
-        var first = "entry.2093447866";
-        var last = "entry.951463637";
-		var email = "entry.993572906";
-        var subject = "entry.1392860080";
-		var message = "entry.1637301940";
-        var baseURL = 'https://docs.google.com/forms/d/e/1FAIpQLScv35MTP43cAWjy8sLdUQm8hAb12q63w6V4zL-Ui5PaUOOWNw/formResponse;
-        var submitRef = '&submit=7337784394560228092';
-        var submitURL = (https://docs.google.com/forms/d/e/1FAIpQLScv35MTP43cAWjy8sLdUQm8hAb12q63w6V4zL-Ui5PaUOOWNw/formResponse/first=firstfield&entry.last=lastfield&entry.email=emailfield&entry.subject=subjectfield&entry.message=messagefield&submit=7337784394560228092;
-		console.log(submitURL);
-        $(this)[0].action=submitURL;
-        $('#input-feedback').text('Thank You!');
-    });
+$('#contactform').one('submit', function() {
+  event.preventDefault();
+  console.log('Submit Button Pressed');
+  var first = $('#firstfield').val();
+  var last = $('#lastfield').val();
+  var email = $('#emailfield').val();
+  var subject = $('#subjectfield').val();
+  var message = $('#messagefield').val();
+  var firstEntry = "entry.2093447866";
+  var lastEntry = "entry.951463637";
+  var emailEntry = "entry.993572906";
+  var subjectEntry = "entry.1392860080";
+  var messageEntry = "entry.1637301940";
+  var baseURL = "https://docs.google.com/forms/d/e/1FAIpQLScv35MTP43cAWjy8sLdUQm8hAb12q63w6V4zL-Ui5PaUOOWNw/formResponse?";
+  var submitRef = '&submit=7337784394560228092';
+  var submitURL = (baseURL + firstEntry + '=' + first + '&' + lastEntry + '=' + last + '&' + emailEntry + '=' + email + '&' + subjectEntry + '=' + subject + '&' + messageEntry + '=' + message + submitRef);
+  console.log(submitURL);
+  $(this)[0].action = submitURL;
+  return (flase);
+});
